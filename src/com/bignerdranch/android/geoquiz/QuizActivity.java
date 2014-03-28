f2b1a49079bacc1c4be67069036b10a45e94e8f8
Jiyi <jiyixx@gmail.com>
2014-03-28 10:45:03 +0800

    第3章完成，3.6调试方面的内容不是很明白

--- src/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ src/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.bignerdranch.android.geoquiz;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -9,47 +10,49 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends Activity {
-	
+
+	private static final String TAG = "QuizActivity";
+	private static final String KEY_INDEX = "index";
 	private Button mTrueButton;
 	private Button mFalseButton;
 	private Button mNextButton;
 	private TextView mQuestionTextView;
-	
+
 	private TrueFalse[] mQuestionBank = new TrueFalse[] {
-		new TrueFalse(R.string.question_oceans, true),
-		new TrueFalse(R.string.question_mideast, false),
-		new TrueFalse(R.string.question_africa, false),
-		new TrueFalse(R.string.question_americas, true),
-		new TrueFalse(R.string.question_asia, true),
-	};
-	
+			new TrueFalse(R.string.question_oceans, true),
+			new TrueFalse(R.string.question_mideast, false),
+			new TrueFalse(R.string.question_africa, false),
+			new TrueFalse(R.string.question_americas, true),
+			new TrueFalse(R.string.question_asia, true), };
+
 	private int mCurrentIndex = 0;
-	
+
 	private void updateQuestion() {
 		int question = mQuestionBank[mCurrentIndex].getQuestion();
 		mQuestionTextView.setText(question);
 	}
-	
+
 	private void checkAnswer(boolean userPressedTrue) {
 		boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
 		int messageResId = 0;
-		
+
 		if (userPressedTrue == answerIsTrue) {
 			messageResId = R.string.correct_toast;
 		} else {
 			messageResId = R.string.incorrect_toast;
 		}
-		
+
 		Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
 	}
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Log.d(TAG, "onCreat(Bundle) called");
 		setContentView(R.layout.activity_quiz);
 
-		mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
-	
+		mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+
 		mTrueButton = (Button) findViewById(R.id.true_button);
 		mTrueButton.setOnClickListener(new View.OnClickListener() {
 
@@ -60,7 +63,7 @@ public class QuizActivity extends Activity {
 		});
 		mFalseButton = (Button) findViewById(R.id.false_button);
 		mFalseButton.setOnClickListener(new View.OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				checkAnswer(false);
@@ -68,14 +71,56 @@ public class QuizActivity extends Activity {
 		});
 		mNextButton = (Button) findViewById(R.id.next_button);
 		mNextButton.setOnClickListener(new View.OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
 				updateQuestion();
 			}
 		});
+
+		if (savedInstanceState != null) {
+			mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+		}
+
 		updateQuestion();
+	} // End of onCreate(Bundle)
+
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		Log.i(TAG, "onSaveInstanceState");
+		outState.putInt(KEY_INDEX, mCurrentIndex);
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+		Log.d(TAG, "onStart() called");
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		Log.d(TAG, "onDestroy() called");
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		Log.d(TAG, "onPause() called");
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		Log.d(TAG, "onResume() called");
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		Log.d(TAG, "onStop() called");
 	}
 
 	@Override
