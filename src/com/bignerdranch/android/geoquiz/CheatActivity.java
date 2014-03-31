b18ad758b07e0be279ea72d1873b5e17e3f2b904
Jiyi <jiyixx@gmail.com>
2014-03-31 14:24:06 +0800

    解决了通过旋转CheatActivity来清除作弊痕迹的漏洞

--- src/com/bignerdranch/android/geoquiz/CheatActivity.java
+++ src/com/bignerdranch/android/geoquiz/CheatActivity.java
@@ -8,43 +8,61 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class CheatActivity extends Activity {
-	
-	public static final String EXTRA_ANSWER_IS_TRUE  = 
+
+	public static final String EXTRA_ANSWER_IS_TRUE = 
 			"com.bignerdranch.android.geoquiz.answer_is_true";
 	public static final String EXTRA_ANSWER_SHOWN = 
 			"com.bignerdranch.android.geoquiz.answer_shown";
-	
+	private static final String KEY_ANSWER_SHOWN = "answer_shown";
+
 	private boolean mAnswerIsTrue;
+	private boolean mAnswerShown = false;
 	private TextView mAnswerTextView;
 	private Button mShowAnswer;
-	
+
 	private void setAnswerShownResult(boolean isAnswerShown) {
 		Intent data = new Intent();
 		data.putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown);
 		setResult(RESULT_OK, data);
 	}
-	
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		outState.putBoolean(KEY_ANSWER_SHOWN, mAnswerShown);
+	}
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_cheat);
-		mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
-		setAnswerShownResult(false);
-		mAnswerTextView = (TextView)findViewById(R.id.answerTextView);
-		mShowAnswer = (Button)findViewById(R.id.showAnswerButton);
+		mAnswerTextView = (TextView) findViewById(R.id.answerTextView);
+		mAnswerIsTrue = getIntent()
+				.getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
+		if (savedInstanceState != null) {
+			mAnswerShown = savedInstanceState.getBoolean(KEY_ANSWER_SHOWN,
+					false);
+		}
+		setAnswerShownResult(mAnswerShown);
+		if (mAnswerShown) {
+			if (mAnswerIsTrue) {
+				mAnswerTextView.setText(R.string.true_button);
+			} else {
+				mAnswerTextView.setText(R.string.false_button);
+			}
+		}
+		mShowAnswer = (Button) findViewById(R.id.showAnswerButton);
 		mShowAnswer.setOnClickListener(new View.OnClickListener() {
-			
 			@Override
 			public void onClick(View arg0) {
-				if(mAnswerIsTrue) {
+				mAnswerShown = true;
+				if (mAnswerIsTrue) {
 					mAnswerTextView.setText(R.string.true_button);
 				} else {
 					mAnswerTextView.setText(R.string.false_button);
 				}
-				setAnswerShownResult(true);
+				setAnswerShownResult(mAnswerShown);
 			}
 		});
 	}
-
-	
 }
--- src/com/bignerdranch/android/geoquiz/TrueFalse.java
+++ src/com/bignerdranch/android/geoquiz/TrueFalse.java
@@ -3,7 +3,7 @@ package com.bignerdranch.android.geoquiz;
 public class TrueFalse {
 	private int mQuestion;
 	private boolean mTrueQuestion;
-	
+
 	public TrueFalse(int question, boolean trueQuestion) {
 		mQuestion = question;
 		mTrueQuestion = trueQuestion;
